1a3d605107c2f21eada3349453222fda6b50c485
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-07-29 21:18:26 +0600

    Backend created and database connected

---
+++ Backend/server.js
@@ -0,0 +1,22 @@
+import express from "express"
+import "dotenv/config"
+import cors from "cors"
+import connectDb from "./configs/db.js";
+
+const app = express()
+const PORT = process.env.PORT || 3000;
+
+await connectDb();
+
+// middlewares 
+app.use(cors())
+app.use(express.json())
+
+
+app.get("/", (req, res)=>{
+    res.send("API is working")
+})
+
+app.listen(PORT, ()=> {
+    console.log(`server is running on PORT ${PORT}`)
+})
